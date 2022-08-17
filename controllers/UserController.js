96fd05cfd9f2c503ecd32dee0580f41a0bcc7252
xenorbil <brunode2001@gmail.com>
2022-08-17 17:25:29 -0300

    working updates

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -21,10 +21,43 @@ class UserController {
 
             btn.disabled = true;
 
-            this.getValues(this.formEl);
+            let values = this.getValues(this.formEl);
+
+            this.getPhoto((content)=>{
+                values.photo = content;
+
+                this.addLine(values);
+
+            });
+
            
         })
     } //fim do escopo onSubmit onSU  
+
+    //metodo com responsabilidade de carregar a foto
+    getPhoto(callback) {
+        //new FileReader = API para carregamento de foto, contendo metodos internos.
+        let fileReader = new FileReader();
+
+        let elements = [...this.formEl.elements].filter(item=>{
+            if (item.name == "photo"){
+                return item;
+            }
+
+            
+        })
+
+        let file = (elements[0].files[0]);
+
+        fileReader.onload = () =>{
+
+            callback(fileReader.result);
+
+        }
+
+        fileReader.readAsDataURL(file);
+
+    }
 
     getValues(formEl) {
 
@@ -35,6 +68,7 @@ class UserController {
             
             if(["name", "email", "password"].indexOf(fields.name) > -1 & !fields.value) {
                 fields.classList.add("errorInput")
+                isValid = false
             }
 
             if(fields.name == "gender") {
@@ -45,21 +79,34 @@ class UserController {
             } else {
                user[fields.name] = fields.value;
             } 
-            })
-            
-            this.addLine(user);
-            
+            });
+
+            if(!isValid) {
+                return false;
+            }
+
+            return new User(
+                user.name,
+                user.gender,
+                user.birth,
+                user.country,
+                user.email,
+                user.admin,
+                user.password,
+                user.photo
+                );
+
     }
 
     addLine(dataUser) {
         let tr = document.createElement("tr");
 
-        tr.innerHTML = `
+        this.tableEl.innerHTML = `
         <tr>
             <td><img src="${dataUser.photo}" alt="" width="30px"></td>
             <td>${dataUser.name}</td>
             <td>${dataUser.email}</td>
-            <td>${dataUser.admin ? "SIM" : "NÃO"}</td>
+            <td>${dataUser.admin}</td>
             <td>${dataUser.register}</td>
             <td>
                 <button class="btn-edit">Editar</button>
@@ -68,6 +115,36 @@ class UserController {
          `;
         document.getElementById("table-users").appendChild(tr)
         console.log(dataUser)
+    }
+
+
+    showPanelCreate() {
+        document.querySelector("#box-user-create").style = "display:block";
+        document.querySelector("#box-user-update").style = "display:none";
+    }
+
+    showPanelUpdate() {
+        document.querySelector("#box-user-create").style = "display:none";
+        document.querySelector("#box-user-update").style = "display:block";
+    }
+
+    updateCount() {
+
+        let numberUsers = 0;
+        let numberAdmins = 0;
+
+        [...this.tableEl.children].forEach(tr=>{
+
+            numberUsers++;
+
+            let user = JSON.parse(tr.dataset.user);
+
+            if(user._admin) numberAdmins++;
+
+        });
+
+        document.querySelector("#number-users").innerHTML = numberUsers;
+        document.querySelector("#number-users-admin").innerHTML = numberAdmins;
     }
 
 };
